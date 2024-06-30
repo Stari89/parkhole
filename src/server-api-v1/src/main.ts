9517d0b2eed300ba98aa0b08f8e4ca618150863b
Damjan Kovačič <letecnez@gmail.com>
2024-06-30 09:47:54 +0200

    Swagger

--- src/server-api-v1/src/main.ts
+++ src/server-api-v1/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
@@ -8,6 +9,15 @@ async function bootstrap() {
     app.enableCors({ origin: true });
     const configService = app.get(ConfigService);
     const appPort = configService.get<string>('APP_PORT');
+    const config = new DocumentBuilder()
+        .setTitle('Parkhole API')
+        .setDescription('server-api-v1')
+        .setVersion('0.0.1')
+        .addBearerAuth()
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
     await app.listen(parseInt(appPort || '3000', 10));
 }
 bootstrap();
